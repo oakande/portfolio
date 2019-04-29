2fab1dd927fd4a82648a155ec1ecbd066b7963d9
Olawale Akande <walemicheal4@gmail.com>
2019-04-29 04:44:13 -0600

    updated fullpage.js

--- js/script.js
+++ js/script.js
@@ -50,3 +50,13 @@ $(window).scroll(function() {
         });
     }
  });
+
+var carousel = $("#carousel").featureCarousel({
+	// include options like this:
+	// (use quotes only for string values, and no trailing comma after last option)
+	// option: value,
+	// option: value
+	captionBelow: true,
+	stopOnHover: true,
+	autoPlay: 7000
+});
